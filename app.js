74b6a542042f627782a4173b83097fa92656eaf7
Scott Endicott <sendicott1391@gmail.com>
2016-09-19 16:37:16 -0400

    Setting things up

---
+++ app.js
@@ -0,0 +1,26 @@
+let testArray = [
+    {
+        question: "What does the Latin phrase 'filioque' mean?",
+        answer: "And the Son",
+        points: 400,
+    },
+
+    {
+        question: "What country was the poet Czeslaw Milosz originally from?",
+        answer: "Poland",
+        points: 300,
+    },
+
+    {
+        question: "Who is, without a doubt, the most hated character in the Star Wars saga?",
+        answer: "Jar Jar Binks",
+        points: 200,
+    },
+
+    {
+        question: "Which author of a famous dystopian novel died on the same day as both President John F. Kennedy and author C.S. Lewis?",
+        answer: "Aldous Huxley",
+        points: 100,
+    },
+];
+
