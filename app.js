aa6fddd28d4741c0b4987138c3f695cf1cc697a4
Scott Endicott <sendicott1391@gmail.com>
2016-09-20 14:00:13 -0400

    Finished hw

--- app.js
+++ app.js
@@ -1,26 +1,75 @@
-let testArray = [
-    {
-        question: "What does the Latin phrase 'filioque' mean?",
-        answer: "And the Son",
-        points: 400,
-    },
+let app = angular.module('Questionnaire', []);
 
-    {
-        question: "What country was the poet Czeslaw Milosz originally from?",
-        answer: "Poland",
-        points: 300,
-    },
+app.controller('QuestionController', function ($scope) {
 
-    {
-        question: "Who is, without a doubt, the most hated character in the Star Wars saga?",
-        answer: "Jar Jar Binks",
-        points: 200,
-    },
+    let testArray = [
+        {
+            question: "What does the Latin phrase 'filioque' mean?",
+            answer: "And the Son",
+            points: 400,
+        },
 
-    {
-        question: "Which author of a famous dystopian novel died on the same day as both President John F. Kennedy and author C.S. Lewis?",
-        answer: "Aldous Huxley",
-        points: 100,
-    },
-];
+        {
+            question: "What country was the poet Czeslaw Milosz originally from?",
+            answer: "Poland",
+            points: 300,
+        },
+
+        {
+            question: "Who is, without a doubt, the most hated character in the Star Wars saga?",
+            answer: "Jar Jar Binks",
+            points: 200,
+        },
+
+        {
+            question: "Which author of a famous dystopian novel died on the same day as both President John F. Kennedy and author C.S. Lewis?",
+            answer: "Aldous Huxley",
+            points: 100,
+        },
+    ];
+
+    let current = 0;
+    $scope.question = testArray[current].question;
+
+    $scope.guessHistory = [];
+
+
+    $scope.nextQuestion = function () {
+        if ($scope.guess === testArray[current].answer) {
+            $scope.score = $scope.score + testArray[current].points;
+            $scope.guessHistory.push(
+                {
+                    theGuess: $scope.guess,
+                    correct: true,
+                    });
+        } else {
+            $scope.score = $scope.score - testArray[current].points;
+            $scope.guessHistory.push(
+                {
+                    theGuess: $scope.guess,
+                    correct: false,
+                    });
+        }
+
+        $scope.guess = "";
+
+        if (current < testArray.length - 1) {
+            current++
+            $scope.question = testArray[current].question;
+        } else {
+            current = 0;
+            $scope.question = testArray[current].question;
+        }
+
+        if ($scope.score < 0) {
+            console.log("How don't you know these??");
+        }
+    }
+
+    $scope.guess = "";
+    $scope.score = 0;
+
+});
+
+
 
